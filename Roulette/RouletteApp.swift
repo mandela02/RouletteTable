aea70799fd22aaf7c5a1071640202e6eabc5f9a7
TriBQ <10khongquang@gmail.com>
2021-10-08 21:48:50 +0700

    Initial Commit

---
+++ Roulette/RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouletteApp.swift
+//  Roulette
+//
+//  Created by TriBQ on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
